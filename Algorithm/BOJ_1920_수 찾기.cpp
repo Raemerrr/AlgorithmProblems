d5a01270e88e5e3316d81b629a8a054d98d88178
Raemin KANG <foals18@gmail.com>
2020-05-14 18:04:48 +0900

    백준 1920번 수 찾기 업로드

---
+++ Algorithm/BOJ_1920_수 찾기.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+
+vector<int> nV;
+vector<int> mV;
+void binarySearch(const int n)
+{
+	int left = 0;
+	int right = nV.size();
+	int mid = right / 2;
+	if (n < nV[mid])
+	{
+		right = mid;
+	}
+	else
+	{
+		left = mid;
+	}
+	bool flag = false;
+	for (int i = left; i < right; i++)
+	{
+		if (nV[i] == n)
+		{
+			flag = true;
+			break;
+		}
+	}
+	if (flag)
+	{
+		cout << "1\n";
+	}
+	else
+	{
+		cout << "0\n";
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> N;
+	nV = vector<int>(N, 0);
+	for (int i = 0; i < N; i++) { cin >> nV[i]; }
+	sort(nV.begin(), nV.end());
+	cin >> M;
+	mV = vector<int>(M, 0);
+	for (int i = 0; i < M; i++) { cin >> mV[i]; }
+	for (const auto& d : mV) { cout << binarySearch(d) << "\n"; }
+	return 0;
+}
