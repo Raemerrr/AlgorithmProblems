03f3cc2995895b33193ac1b2d751bb759a2c9b1d
Raemin KANG <foals18@gmail.com>
2020-08-12 20:26:44 +0900

    백준 1238번 파티 업로드

--- Algorithm/BOJ_1238_파티.cpp
+++ Algorithm/BOJ_1238_파티.cpp
@@ -4,34 +4,26 @@
 
 using namespace std;
 
-const int INF = 100000000;
-int N, nodeCnt;
+const int INF = 200000000;
+int N, M, X, ans;
 vector<vector<int>> v;
 vector<vector<int>> dp;
-
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
-	cin >> N;
-	cin >> nodeCnt;
+	cin >> N >> M >> X;
 	v = vector<vector<int>>(N + 1, vector<int>(N + 1, 0));
 	dp = vector<vector<int>>(N + 1, vector<int>(N + 1, 0));
-	for (int i = 0; i < nodeCnt; i++)
+	//단방향 입력
+	for (int i = 0; i < M; i++)
 	{
-		int a = 0, b = 0, cost = 0;
-		cin >> a >> b >> cost;
-		//문제에서 말하는 "시작 도시와 도착 도시를 연결하는 노선은 하나가 아닐 수 있다." 비용이 적은 노선만 생각하기위해
-		if (v[a][b] != 0)
-		{
-			v[a][b] = min(v[a][b], cost);
-		}
-		else
-		{
-			v[a][b] = cost;
-		}
+		int num1 = 0, num2 = 0, cost = 0;
+		cin >> num1 >> num2 >> cost;
+		v[num1][num2] = cost;
 	}
 
+	//dp 테이블 세팅
 	for (int i = 1; i <= N; i++)
 	{
 		for (int j = 1; j <= N; j++)
@@ -50,6 +42,7 @@ int main()
 		}
 	}
 
+	//단방향으로 가장 가까운 거리로 도달 할 수 있는 경우 구하기
 	for (int k = 1; k <= N; k++)
 	{
 		for (int i = 1; i <= N; i++)
@@ -64,20 +57,11 @@ int main()
 		}
 	}
 
+	//출발과 돌아오는 값을 더해서 가장 큰 값을 구한다.
 	for (int i = 1; i <= N; i++)
 	{
-		for (int j = 1; j <= N; j++)
-		{
-			//도착 할 수 없는 곳
-			if (dp[i][j] == INF)
-			{
-				cout << "0 ";
-			}
-			else
-			{
-				cout << dp[i][j] << " ";
-			}
-		}
-		cout << "\n";
+		ans = max(dp[i][X] + dp[X][i], ans);
 	}
+	cout << ans << "\n";
+	return 0;
 }
