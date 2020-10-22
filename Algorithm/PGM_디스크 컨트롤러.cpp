ebd2d96c4e13eb46246de09b5a523acebbb580c3
Raemin KANG <foals18@gmail.com>
2020-10-23 07:04:18 +0900

    프로그래머스 디스크 컨트롤 수정 업로드

--- Algorithm/PGM_디스크 컨트롤러.cpp
+++ Algorithm/PGM_디스크 컨트롤러.cpp
@@ -1,13 +1,14 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <queue>
 using namespace std;
 
 int solution(vector<vector<int>> jobs) {
 	int size = jobs.size();
 	int answer = 0;
 	int time = 0;
-	vector<pair<int, int>> wait;
+	priority_queue<pair<int, int>> wait;
 	sort(jobs.begin(), jobs.end(), [](const vector<int>& a, const vector<int>& b)
 	{
 		return a[0] < b[0];
@@ -16,7 +17,7 @@ int solution(vector<vector<int>> jobs) {
 	{
 		while (!jobs.empty() && jobs[0][0] <= time)
 		{
-			wait.emplace_back(jobs[0][1], jobs[0][0]);
+			wait.emplace(-jobs[0][1], jobs[0][0]);
 			jobs.erase(jobs.begin());
 		}
 		if (wait.empty() && !jobs.empty())
@@ -25,12 +26,10 @@ int solution(vector<vector<int>> jobs) {
 		}
 		else if (!wait.empty())
 		{
-			sort(wait.begin(), wait.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
-				return a.first < b.first;
-			});
-			answer += ((time + wait[0].first) - wait[0].second);
-			time += wait[0].first;
-			wait.erase(wait.begin());
+			pair<int, int> top = wait.top();
+			answer += ((time + (-top.first)) - top.second);
+			time += -top.first;
+			wait.pop();
 		}
 	}
 	return answer / size;
