22c3a46c9ea70d177a60a60a91140e5c37f206ac
Raemin KANG <foals18@gmail.com>
2021-04-01 10:46:06 +0900

    백준 15961번 회전 초밥 재업로드

--- Algorithm/BOJ_15961_회전 초밥_210331.cpp
+++ Algorithm/BOJ_15961_회전 초밥_210331.cpp
@@ -1,33 +1,66 @@
 #include <iostream>
+#include <vector>
 #include <deque>
-#include <algorithm>
+
 using namespace std;
 
-int arr[3000001];
-int sushi[3001];
+int N, d, k, c;
+vector<int> v;
+int s, e, cnt, ans;
+vector<int> arr;
 
 int main() {
-	ios::sync_with_stdio(0); cin.tie(0);
-	int n, d, k, c; cin >> n >> d >> k >> c;
-	for (int i = 1; i <= n; ++i) cin >> arr[i];
-
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> N >> d >> k >> c;
+	v.assign(N, 0);
+	arr.assign(3001, 0);
 	deque<int> dq;
-	int cnt = 0, ans = 0;
-	for (int i = 1; i <= k; ++i) {
-		if (!sushi[arr[i]]) cnt++;
-		sushi[arr[i]]++; dq.push_back(arr[i]);
+	for (int i = 0; i < N; i++)
+	{
+		cin >> v[i];
 	}
-
-	for (int s = 1, e = k + 1; s <= n; ++s, ++e) {
-		if (e == n + 1) e = 1;
-		ans = max(ans, sushi[c] ? cnt : cnt + 1);
-
-		sushi[arr[s]]--; dq.pop_front();
-		if (!sushi[arr[s]]) cnt--;
-
-		if (!sushi[arr[e]]) cnt++;
-		sushi[arr[e]]++; dq.push_back(arr[e]);
+	//k까지의 내용을 미리 채워놓는다.
+	for (int i = 0; i < k; i++)
+	{
+		if (arr[v[i]] == 0)
+		{
+			cnt++;
+		}
+		dq.push_back(v[i]);
+		arr[v[i]]++;
 	}
-
-	cout << ans;
+	//시작 위치
+	s = 0;
+	//끝 위치
+	e = k - 1;
+	while (s < N)
+	{
+		//쿠폰 초밥 포함 확인
+		int tCnt = (arr[c] > 0) ? cnt : cnt + 1;
+		ans = (ans > tCnt) ? ans : tCnt;
+		e++;
+		s++;
+		//가장 끝에 위치한 초밥 이후 0번 초밥으로 접근 하도록
+		if (e == N)
+		{
+			e = 0;
+		}
+		//뒤에 것을 추가하는 작업
+		if (arr[v[e]] == 0)
+		{
+			cnt++;
+		}
+		arr[v[e]]++;
+		dq.push_back(v[e]);
+		//앞에 것을 빼는 작업
+		arr[dq.front()]--;
+		if (arr[dq.front()] == 0)
+		{
+			cnt--;
+		}
+		dq.pop_front();
+	}
+	cout << ans << "\n";
+	return 0;
 }
