c93a19f4efc4b189519b2a008c923fbe1db8b8c5
Raemin KANG <foals18@gmail.com>
2021-06-07 07:31:28 +0900

    백준 11969번 Breed Counting 재업로드

--- Algorithm/BOJ_11969_Breed Counting_210607.cpp
+++ Algorithm/BOJ_11969_Breed Counting_210607.cpp
@@ -4,38 +4,31 @@
 using namespace std;
 
 int N, M;
-vector<int> v;
 vector<int> sum1;
 vector<int> sum2;
 vector<int> sum3;
+
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 	cin >> N >> M;
-	v.assign(N, 0);
 	sum1.assign(N, 0);
 	sum2.assign(N, 0);
 	sum3.assign(N, 0);
 	for (int i = 0; i < N; i++)
 	{
-		cin >> v[i];
-		if (v[i] == 1)
+		int num = 0; cin >> num;
+		if (i == 0)
 		{
-			sum1[i] += (i == 0) ? 1 : sum1[i - 1] + 1;
-			sum2[i] += (i == 0) ? 0 : sum2[i - 1];
-			sum3[i] += (i == 0) ? 0 : sum3[i - 1];
-		}
-		else if (v[i] == 2)
-		{
-			sum1[i] += (i == 0) ? 0 : sum1[i - 1];
-			sum2[i] += (i == 0) ? 1 : sum2[i - 1] + 1;
-			sum3[i] += (i == 0) ? 0 : sum3[i - 1];
+			sum1[i] += ((num == 1) ? 1 : 0);
+			sum2[i] += ((num == 2) ? 1 : 0);
+			sum3[i] += ((num == 3) ? 1 : 0);
 		}
 		else
 		{
-			sum1[i] += (i == 0) ? 0 : sum1[i - 1];
-			sum2[i] += (i == 0) ? 0 : sum2[i - 1];
-			sum3[i] += (i == 0) ? 1 : sum3[i - 1] + 1;
+			sum1[i] += sum1[i - 1] + ((num == 1) ? 1 : 0);
+			sum2[i] += sum2[i - 1] + ((num == 2) ? 1 : 0);
+			sum3[i] += sum3[i - 1] + ((num == 3) ? 1 : 0);
 		}
 	}
 	while (M--)
