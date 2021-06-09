f3364eeb3233755c5a267737c915c6cd396bd1e8
Raemin KANG <foals18@gmail.com>
2021-06-10 07:20:55 +0900

    백준 2015번 수들의 합 4 업로드

--- Algorithm/BOJ_2632_피자판매_210609.cpp
+++ Algorithm/BOJ_2632_피자판매_210609.cpp
@@ -43,18 +43,17 @@ int main() {
 	Proc(bSum, B, M, false);
 	for (const int d : s) {
 		int tempCnt = 0;
-		if (d >= K)
-		{
-			break;
-		}
+		//합이 이미 K와 같거나 큰 경우는 계산 필요 없다.
+		if (d >= K) break;
+		//'A' 피자 합 중 몇개가 존재하는지
 		tempCnt = (upper_bound(aSum.begin(), aSum.end(), d) - lower_bound(aSum.begin(), aSum.end(), d));
+		//'B' 피자 합 중 K - d(현재 'A' 피자 합)을 만족하는 수가 있는지
 		auto it = lower_bound(bSum.begin(), bSum.end(), K - d);
 		if (it != bSum.end() && (*it) == K - d)
 		{
 			tempCnt *= (upper_bound(bSum.begin(), bSum.end(), K - d) - it);
 			answer += tempCnt;
 		}
-
 	}
 	cout << answer << "\n";
 	return 0;
