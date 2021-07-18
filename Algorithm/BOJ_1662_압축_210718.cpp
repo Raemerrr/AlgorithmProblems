965a71e41511a3b526515ef7ca1cc66e37abb317
Raemin KANG <foals18@gmail.com>
2021-07-18 13:00:25 +0900

    백준 1662번 압축 업로드

---
+++ Algorithm/BOJ_1662_압축_210718.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<bool> visited;
+
+int dfs(string& S, int idx) {
+	int cnt = 0;
+
+	for (int i = idx; i < S.size(); i++) {
+		if (S[i] == '(' && !visited[i]) {
+			visited[i] = true;
+			int num = S[i - 1] - '0';
+			cnt--;
+			cnt += num * dfs(S, i + 1);
+		}
+		else if (S[i] == ')' && !visited[i]) {
+			visited[i] = true;
+			return cnt;
+		}
+		else if (!visited[i]) {
+			visited[i] = true;
+			cnt++;
+		}
+	}
+
+	return cnt;
+}
+
+int main() {
+	int T;
+	int N, M, K, H;
+	int X, Y;
+	string S, answer; cin >> S;
+	visited.assign(S.size(), false);
+	cout << dfs(S, 0) << endl;
+	return 0;
+}
