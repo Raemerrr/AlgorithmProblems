890c8ce012ad620c980820072833701d42a43b5c
Raemin KANG <foals18@gmail.com>
2021-10-29 09:13:49 +0900

    프로그래머스 - 정렬 - H-Index 재업로드

--- Algorithm/PGM_정렬_H-INDEX_211029.cpp
+++ Algorithm/PGM_정렬_H-INDEX_211029.cpp
@@ -7,13 +7,7 @@ using namespace std;
 int solution(vector<int> citations) {
 	int answer = 0;
 	sort(citations.rbegin(), citations.rend());
-	for (int i = 0; i < citations.size(); i++)
-	{
-		if (i + 1 <= citations[i])
-		{
-			answer++;
-		}
-	}
+	while (answer < citations.size() && answer + 1 <= citations[answer]) answer++;
 	return answer;
 }
 
