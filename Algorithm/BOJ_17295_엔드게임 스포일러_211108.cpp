b818279271c3a4d0a080f3433a57b84f55ec8eb1
Raemin KANG <foals18@gmail.com>
2021-11-08 06:49:45 +0900

    백준 17295번 엔드게임 스포일러 업로드

---
+++ Algorithm/BOJ_17295_엔드게임 스포일러_211108.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	string str; cin >> str;
+	cout << "Avengers: Endgame\n";
+	return 0;
+}
